f5748ac65608cc785135b62da760165c46d9a6af
thiago dias <thiagoddsm@gmail.com>
2025-09-20 15:14:31 +0000

    Não est´abirndo o chat com gemini

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,29 @@ const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['exceljs'],
   },
+  async headers() {
+    const csp = `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' *.cloudworkstations.googleusercontent.com vscode-cdn.net;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data:;
+      font-src 'self';
+      connect-src 'self';
+      frame-src 'self';
+    `.replace(/\s{2,}/g, ' ').trim();
+
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: csp,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
